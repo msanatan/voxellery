1beeebc076f24f97cace5e4e5cd26eb008b7dcd9
Marcus Sanatan <msanatan@gmail.com>
2024-08-21 12:17:50 -0400

    Add a model for the artwork
    I do want to tweak the user model a bit, let's learn how to do that!

--- backend/artworks/models.py
+++ backend/artworks/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Artwork(models.Model):
+    # Use UUIDs which work better for distributed systems
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    url = models.CharField("Media URL", max_length=1024, null=False, blank=False)
+    title = models.CharField("Title", max_length=256, null=False, blank=False)
+    alt_text = models.TextField("Alt Text", null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
