301ca3e7844d65dcc7a525b3e8535ebc4183d725
Marcus Sanatan <msanatan@gmail.com>
2024-08-21 10:49:30 -0400

    Reorder app init

--- backend/voxellery/urls.py
+++ backend/voxellery/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("artworks/", include("artworks.urls")),
     path('admin/', admin.site.urls),
+    path("artworks/", include("artworks.urls")),
 ]
