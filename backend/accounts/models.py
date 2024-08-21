4b6292fec83d8cc526bc3ca5ef54b7deabc39d0d
Marcus Sanatan <msanatan@gmail.com>
2024-08-21 18:28:26 -0400

    Set up a custom user
    We created a custom one so that email is used for authentication instead of username.
    It's always advisable to extend AbstractUser and use a custom one.
    However, when you want to change how it authenticates - a little more involved.

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,70 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+class VoxelleryUserManager(BaseUserManager):
+    def create_user(self, email: str, username: str, password: str | None=None, **extra_fields: dict):
+        '''
+        Creates and saves a user with an email, username and password
+        '''
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(email=self.normalize_email(email), username=username,**extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email: str, username: str, password: str | None=None, **extra_fields: dict):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            email=self.normalize_email(email),
+            username=username,
+            password=password,
+            **extra_fields
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+class VoxelleryUser(AbstractBaseUser):
+    '''
+    In this custom user model, usernames are still required but we login via email.
+    '''
+    # Use UUIDs which work better for distributed systems
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(verbose_name="Email Address", max_length=255, unique=True)
+    # A public display name for users, as we don't want to expose emails
+    username = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    # Tell Django that our user's unique identifier is their email
+    USERNAME_FIELD = 'email'
+    # Keep username required
+    REQUIRED_FIELDS = ['username']
+
+    # Needed for the manage.py createsuperuser command to work
+    objects = VoxelleryUserManager()
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        '''Should be more specific, for now let's allow all'''
+        return True
+
+    def has_module_perms(self, app_label):
+        '''Should be more specific, for now let's allow all'''
+        return True
+
+    @property
+    def is_staff(self):
+        '''All admins are staff'''
+        return self.is_admin
