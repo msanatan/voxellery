42c3349fef8eb264d02ea74e6ba78bcdc69f3c8a
Marcus Sanatan <msanatan@gmail.com>
2024-08-22 13:28:27 -0400

    Use the new permission for the Artworks API

--- backend/artworks/views.py
+++ backend/artworks/views.py
@@ -1,9 +1,11 @@
 from rest_framework import viewsets
 
 from .models import Artwork
+from .permissions import IsOwnerOrReadOnly
 from .serializers import ArtworkSerializer
 
 
 class ArtworkViewSet(viewsets.ModelViewSet):
     queryset = Artwork.objects.all()
     serializer_class = ArtworkSerializer
+    permission_classes = [IsOwnerOrReadOnly]
