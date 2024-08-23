7efaaa58f37ae735c423fd802da5e1b196431de8
Marcus Sanatan <msanatan@gmail.com>
2024-08-23 02:06:03 -0400

    First API call - login and get tokens
    We need to save this in state, a context in particular.
    Things should happen once logged in:
    
    - Buttons changes to logout (new endpoint)
    - Upload image button is enabled

--- frontend/components/login/loginbutton.tsx
+++ frontend/components/login/loginbutton.tsx
@@ -1,3 +1,4 @@
+import { login } from "@/services/accounts";
 import {
   Button,
   Popover,
@@ -17,8 +18,43 @@ import {
   Input,
   FormHelperText,
 } from "@chakra-ui/react";
+import { ChangeEvent, useState } from "react";
 
 export default function LoginButton() {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  async function onLoginHandler(
+    event: MouseEvent<HTMLButtonElement, MouseEvent>
+  ): void {
+    try {
+      const response = await login(email, password);
+      console.log(response);
+    } catch (error) {
+      console.error(`Got this error\n${error}`);
+    }
+  }
+
+  function handleEmailChange(event: ChangeEvent<HTMLInputElement>): void {
+    setEmail(event.target.value);
+  }
+
+  function handleUsernameChange(event: ChangeEvent<HTMLInputElement>): void {
+    setUsername(event.target.value);
+  }
+
+  function handlePasswordChange(event: ChangeEvent<HTMLInputElement>): void {
+    setPassword(event.target.value);
+  }
+
+  function handleConfirmPasswordChange(
+    event: ChangeEvent<HTMLInputElement>
+  ): void {
+    setConfirmPassword(event.target.value);
+  }
+
   return (
     <Popover>
       <PopoverTrigger>
@@ -49,28 +85,48 @@ export default function LoginButton() {
               <TabPanel>
                 <FormControl mt={4}>
                   <FormLabel>Email address</FormLabel>
-                  <Input type="email" name="email" />
+                  <Input
+                    type="email"
+                    name="email"
+                    value={email}
+                    onChange={handleEmailChange}
+                  />
                 </FormControl>
 
                 <FormControl mt={4}>
                   <FormLabel>Password</FormLabel>
-                  <Input type="password" name="password" />
+                  <Input
+                    type="password"
+                    name="password"
+                    value={password}
+                    onChange={handlePasswordChange}
+                  />
                 </FormControl>
 
-                <Button mt={4} colorScheme="teal">
+                <Button mt={4} colorScheme="teal" onClick={onLoginHandler}>
                   Submit
                 </Button>
               </TabPanel>
               <TabPanel>
                 <FormControl mt={4}>
                   <FormLabel>Email address</FormLabel>
-                  <Input type="email" name="email" />
+                  <Input
+                    type="email"
+                    name="email"
+                    value={email}
+                    onChange={handleEmailChange}
+                  />
                   <FormHelperText>We'll never share your email.</FormHelperText>
                 </FormControl>
 
                 <FormControl mt={4}>
                   <FormLabel>Username</FormLabel>
-                  <Input type="text" name="username" />
+                  <Input
+                    type="text"
+                    name="username"
+                    value={username}
+                    onChange={handleUsernameChange}
+                  />
                   <FormHelperText>
                     This is how users will see you
                   </FormHelperText>
@@ -78,12 +134,22 @@ export default function LoginButton() {
 
                 <FormControl mt={4}>
                   <FormLabel>Password</FormLabel>
-                  <Input type="password" name="password" />
+                  <Input
+                    type="password"
+                    name="password"
+                    value={password}
+                    onChange={handlePasswordChange}
+                  />
                 </FormControl>
 
                 <FormControl mt={4}>
                   <FormLabel>Confirm Password</FormLabel>
-                  <Input type="password" name="confirmPassword" />
+                  <Input
+                    type="password"
+                    name="confirmPassword"
+                    value={confirmPassword}
+                    onChange={handleConfirmPasswordChange}
+                  />
                 </FormControl>
                 <Button mt={4} colorScheme="teal">
                   Submit
