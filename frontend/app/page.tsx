5ef17c60a2452727241d4484fe9935f14270f7a8
Marcus Sanatan <msanatan@gmail.com>
2024-08-22 23:31:13 -0400

    Set up a basic grid to display images

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,13 @@
-import Image from "next/image";
+import { Box, SimpleGrid } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <SimpleGrid columns={[1, 3]} spacing={4} px={[4, 16]} mt={4}>
+      <Box bg="tomato" height="80px"></Box>
+      <Box bg="tomato" height="80px"></Box>
+      <Box bg="tomato" height="80px"></Box>
+      <Box bg="tomato" height="80px"></Box>
+      <Box bg="tomato" height="80px"></Box>
+    </SimpleGrid>
   );
 }
