be9fdb98e93691614537921482b555ae0f74183a
Marcus Sanatan <msanatan@gmail.com>
2024-08-23 02:02:38 -0400

    Allow anyone to login and sign up
    We also return credentials after sign up

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics, status
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 
@@ -8,9 +9,19 @@ from .serializers import VoxelleryUserSerializer
 class SignUpView(generics.CreateAPIView):
     queryset = VoxelleryUser.objects.all()
     serializer_class = VoxelleryUserSerializer
+    permission_classes = [AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        user = self.get_object()  # Get the newly created user
+        refresh = RefreshToken.for_user(user)
+        response.data['refresh'] = str(refresh)
+        response.data['access'] = str(refresh.access_token)
+        return response
 
 class LoginView(generics.GenericAPIView):
     serializer_class = VoxelleryUserSerializer
+    permission_classes = [AllowAny]
 
     def post(self, request, *args, **kwargs):
         email = request.data.get('email')
