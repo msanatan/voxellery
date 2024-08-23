afd551ffd8a93c350cfe76df789ddd54ae1e7d1f
Marcus Sanatan <msanatan@gmail.com>
2024-08-22 20:13:59 -0400

    Integrate toaster with app

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/common/navbar";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +22,10 @@ export default function RootLayout({
         <div className="flex flex-col">
           <NavBar />
         </div>
-        <main className="flex-grow px-4 sm:px-6 py-3">{children}</main>
+        <main className="flex-grow px-4 sm:px-6 py-3">
+          {children}
+          <Toaster position="bottom-center" />
+        </main>
       </body>
     </html>
   );
