7c9301a4d479807c9c932e74c7f023643735775a
Marcus Sanatan <msanatan@gmail.com>
2024-08-23 02:02:59 -0400

    Add a placeholder card on the homepage
    Helps see how it looks

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,13 +1,15 @@
-import { Box, SimpleGrid } from "@chakra-ui/react";
+import Artwork from "@/components/gallery/artwork";
+import { SimpleGrid } from "@chakra-ui/react";
 
 export default function Home() {
   return (
     <SimpleGrid columns={[1, 3]} spacing={4} px={[4, 16]} mt={4}>
-      <Box bg="tomato" height="80px"></Box>
-      <Box bg="tomato" height="80px"></Box>
-      <Box bg="tomato" height="80px"></Box>
-      <Box bg="tomato" height="80px"></Box>
-      <Box bg="tomato" height="80px"></Box>
+      <Artwork
+        title="Test"
+        altText="A test image"
+        url="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+        username="msanatan"
+      />
     </SimpleGrid>
   );
 }
