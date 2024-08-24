0f321a2a34d1fc7cfe3167bdb06307ea9bb5eff5
Marcus Sanatan <msanatan@gmail.com>
2024-08-23 21:29:27 -0400

    Ensure the authprovider is added to the app so we can use it

--- frontend/app/providers.tsx
+++ frontend/app/providers.tsx
@@ -1,7 +1,12 @@
 "use client";
 
+import { AuthProvider } from "@/hooks/useAuth";
 import { ChakraProvider } from "@chakra-ui/react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ChakraProvider>{children}</ChakraProvider>;
+  return (
+    <ChakraProvider>
+      <AuthProvider>{children}</AuthProvider>
+    </ChakraProvider>
+  );
 }
