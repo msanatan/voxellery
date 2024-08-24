a6eb0526f74feef6d057c42a2b43af2beab29496
Marcus Sanatan <msanatan@gmail.com>
2024-08-23 21:19:10 -0400

    Create an axios instance for this backend API
    The cool thing is that it automatically sends authenticated requests once we have the token.
    In theory, will need to test it.

---
+++ frontend/utils/backendApiClient.ts
@@ -0,0 +1,27 @@
+import { useAuth } from "@/hooks/useAuth";
+import axios from "axios";
+
+const backendApiClient = axios.create({
+  baseURL: "http://localhost:8000/api",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
+
+backendApiClient.interceptors.request.use(
+  (config) => {
+    const { accessToken } = useAuth(); // Get accessToken from your custom useAuth hook
+
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default backendApiClient;
