6e328dc2e47b201eff95e0e16dc0dfad3c62e3e1
Marcus Sanatan <msanatan@gmail.com>
2024-08-23 22:49:37 -0400

    Allow us to get the user from the auth token

--- backend/artworks/views.py
+++ backend/artworks/views.py
@@ -1,11 +1,15 @@
 from rest_framework import viewsets
-
 from .models import Artwork
 from .permissions import IsOwnerOrReadOnly
 from .serializers import ArtworkSerializer
-
 
 class ArtworkViewSet(viewsets.ModelViewSet):
     queryset = Artwork.objects.all()
     serializer_class = ArtworkSerializer
     permission_classes = [IsOwnerOrReadOnly]
+
+    def get_serializer_context(self):
+        '''Pass the request context to the serializer to access the user'''
+        context = super().get_serializer_context()
+        context['request'] = self.request
+        return context
